11d18cf80393f007e2528d3f20a29c407ebf9d91
Prerna <prernakumari10260@gmail.com>
2026-09-23 16:37:07 +0530

    ques 1

---
+++ Ques1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+  public class Ques1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println( "enter a number:");
+        int a =sc.nextInt();
+        System.out.println( "enter a number:");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("sum:"+sum);
+
+    }
+}
