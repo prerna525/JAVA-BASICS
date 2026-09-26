24a9861d10aa272e2e8f5fcaedffa757349a0232
Prerna <prernakumari10260@gmail.com>
2026-09-26 14:57:37 +0530

    sum of all num

---
+++ ques12.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ques12 {
+    public static void main(String[]args)
+    {
+        for(int i=2;i<=30;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
