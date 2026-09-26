c58c9a3cb2ca8f5c71140757d9f248336570309b
Prerna <prernakumari10260@gmail.com>
2026-09-26 14:58:26 +0530

    factorial

---
+++ ques14.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ques14 {
+    public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    int fact =1;
+    for(int i=5;i>=1;i--){
+        fact=fact*i;
+    }
+
+    System.out.println(fact);
+}}
